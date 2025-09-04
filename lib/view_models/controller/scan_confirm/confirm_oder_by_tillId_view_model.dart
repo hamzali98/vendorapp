10a499acb93c35a12ee14a44d12620907261e093
Hamza Ali <hamza.ali.art3@gmail.com>
2025-09-05 02:04:50 +0500

    cleaning

--- lib/view_models/controller/scan_confirm/confirm_oder_by_tillId_view_model.dart
+++ lib/view_models/controller/scan_confirm/confirm_oder_by_tillId_view_model.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
-import '../../../data/response/status.dart';
-import '../../../models/scan_confirm/scan_till_id_confirm_order_model.dart';
-import '../../../repository/scan_confirm/scan_confirm_id_repository.dart';
+import 'package:vendor_app/data/response/status.dart';
+import 'package:vendor_app/models/scan_confirm/scan_till_id_confirm_order_model.dart';
+import 'package:vendor_app/repository/scan_confirm/scan_confirm_id_repository.dart';
 
 class ConfirmOrderByIdController extends GetxController {
   final _api = ScanAndConfirmRepository();
@@ -18,22 +19,32 @@ class ConfirmOrderByIdController extends GetxController {
   /// 🔍 **Confirm Order by Order Q ID**
   Future<void> confirmOrder(String orderQId) async {
     setRxRequestStatus(Status.LOADING);
-    print("📡 DEBUG: Fetching Order with order_q_id: $orderQId");
+    if (kDebugMode) {
+      print("📡 DEBUG: Fetching Order with order_q_id: $orderQId");
+    }
 
     try {
       final response = await _api.ConfirmOrderByTillID(orderQId);
-      print("🔎 API Response: ${response.toJson()}");
+      if (kDebugMode) {
+        print("🔎 API Response: ${response.toJson()}");
+      }
 
       if (response.orderDetails != null) {
-        print("✅ Order Found: ${response.orderDetails!.orderId}");
+        if (kDebugMode) {
+          print("✅ Order Found: ${response.orderDetails!.orderId}");
+        }
         setOrderData(response);
         setRxRequestStatus(Status.COMPLETED);
       } else {
-        print("❌ Order Not Found!");
+        if (kDebugMode) {
+          print("❌ Order Not Found!");
+        }
         setRxRequestStatus(Status.ERROR);
       }
     } catch (error) {
-      print("❌ API Error: $error");
+      if (kDebugMode) {
+        print("❌ API Error: $error");
+      }
       setRxRequestStatus(Status.ERROR);
     }
   }
